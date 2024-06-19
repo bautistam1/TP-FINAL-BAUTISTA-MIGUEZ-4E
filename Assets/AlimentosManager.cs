22b7ffd4505a6a5b032a466b1c20174f1f9fb54f
MiguezB <bautimig08@gmail.com>
2024-06-19 15:34:41 -0300

    Initial commit

---
+++ Assets/AlimentosManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlimentosManager : MonoBehaviour
+{
+    public GameObject[] Alimentos;
+    public int currentIndex = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DeactivateAll();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ActivateAll();
+        }
+
+     
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                DeactivateAll();
+                currentIndex--;
+                if (currentIndex < 0)
+                {
+                    currentIndex = Alimentos.Length - 1;
+                }
+                Alimentos[currentIndex].SetActive(true);
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                DeactivateAll();
+                currentIndex++;
+                if (currentIndex >= Alimentos.Length)
+                {
+                    currentIndex = 0;
+                }
+                Alimentos[currentIndex].SetActive(true);
+            }
+
+
+
+        }
+        void DeactivateAll()
+        {
+            for (int i = 0; i < Alimentos.Length; i++)
+            {
+                Alimentos[i].SetActive(false);
+            }
+        }
+
+        void ActivateAll()
+        {
+            for (int i = 0; i < Alimentos.Length; i++)
+            {
+                Alimentos[i].SetActive(true);
+            }
+        }
+
+        void ActivateByIndex(int index)
+        {
+            Alimentos[index].SetActive(true);
+        }
+    
+}
